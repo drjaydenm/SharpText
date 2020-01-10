b50dde5cd9d5b83d2f874685a1ecf53d6b4a02a6
Jayden M <drjaydenm@hotmail.com>
2020-01-10 15:29:55 +1000

    Added functionality to change font and color

--- Veldrid.TextRendering/Program.cs
+++ Veldrid.TextRendering/Program.cs
@@ -15,6 +15,21 @@ namespace Veldrid.TextRendering
         private static TextRenderer textRenderer;
         private static float letterSpacing = 1;
         private static float fontSize = 20;
+        private static int currentFontIndex = 0;
+        private static string[] fonts =
+        {
+            "Fonts/OpenSans-Regular.woff",
+            "Fonts/LeArchitect.ttf",
+            "Fonts/neon2.ttf"
+        };
+        private static int currentColorIndex = 0;
+        private static RgbaFloat[] colors =
+        {
+            RgbaFloat.Black,
+            RgbaFloat.Red,
+            RgbaFloat.Green,
+            RgbaFloat.CornflowerBlue
+        };
 
         public static void Main(string[] args)
         {
@@ -37,7 +52,7 @@ namespace Veldrid.TextRendering
             factory = graphicsDevice.ResourceFactory;
             commandList = factory.CreateCommandList();
 
-            var font = new Font("Fonts/OpenSans-Regular.woff", fontSize);
+            var font = new Font(fonts[currentFontIndex], fontSize);
             textRenderer = new TextRenderer(graphicsDevice, font);
         }
 
@@ -50,12 +65,12 @@ namespace Veldrid.TextRendering
             if (inputSnapshot.KeyEvents.Any(ke => ke.Key == Key.Up && ke.Down))
             {
                 fontSize += 0.5f;
-                textRenderer.UpdateFont(new Font("Fonts/OpenSans-Regular.woff", fontSize));
+                UpdateFont();
             }
             if (inputSnapshot.KeyEvents.Any(ke => ke.Key == Key.Down && ke.Down))
             {
                 fontSize -= 0.5f;
-                textRenderer.UpdateFont(new Font("Fonts/OpenSans-Regular.woff", fontSize));
+                UpdateFont();
             }
             if (inputSnapshot.KeyEvents.Any(ke => ke.Key == Key.Left && ke.Down))
             {
@@ -65,8 +80,17 @@ namespace Veldrid.TextRendering
             {
                 letterSpacing += 0.01f;
             }
+            if (inputSnapshot.KeyEvents.Any(ke => ke.Key == Key.Enter && ke.Down))
+            {
+                currentFontIndex = (currentFontIndex + 1) % fonts.Length;
+                UpdateFont();
+            }
+            if (inputSnapshot.KeyEvents.Any(ke => ke.Key == Key.Space && ke.Down))
+            {
+                currentColorIndex = (currentColorIndex + 1) % colors.Length;
+            }
 
-            textRenderer.DrawText("Sixty zippers were quickly picked from the woven jute bag.", Vector2.Zero, RgbaFloat.DarkRed, letterSpacing);
+            textRenderer.DrawText("Sixty zippers were quickly picked from the woven jute bag.", Vector2.Zero, colors[currentColorIndex], letterSpacing);
         }
 
         private static void Draw()
@@ -110,6 +134,11 @@ namespace Veldrid.TextRendering
                 preferStandardClipSpaceYDirection: true,
                 swapchainSrgbFormat: true
             ));
+        }
+
+        private static void UpdateFont()
+        {
+            textRenderer.UpdateFont(new Font(fonts[currentFontIndex], fontSize));
         }
     }
 }
